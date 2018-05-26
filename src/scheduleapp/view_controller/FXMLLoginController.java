2cffd53de5002ea32562874f51671a96b0b82dac
Jens Larsen <dasuperman@gmail.com>
2018-05-25 18:01:10 -0700

    Add Login screen controller

--- src/scheduleapp/view_controller/FXMLLoginController.java
+++ src/scheduleapp/view_controller/FXMLLoginController.java
@@ -7,21 +7,39 @@ package scheduleapp.view_controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author dasuperman
  */
-public class FXMLLoginController implements Initializable {
+public class FXMLLoginController extends Application {
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void start(Stage primaryStage) throws Exception {
+        Stage stage = primaryStage;
+        
+        FXMLLoader mainController = new FXMLLoader();
+        
+        mainController.setLocation(getClass().getResource("FXMLLogin.fxml"));
+        
+        Parent root = mainController.load();
+        
+        stage.setScene(new Scene(root));
+        
+        stage.setTitle("Login");
+        stage.setResizable(false);
+        stage.show();
+    }
     
+    public static void main(String[] args) {
+        launch(args);
+    }  
 }
