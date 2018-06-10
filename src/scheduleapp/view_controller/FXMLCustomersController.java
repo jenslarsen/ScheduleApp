183aad0d0cdccde7b4cdd7dd9b22743cee29d98e
Jens Larsen <dasuperman@gmail.com>
2018-06-10 10:32:16 -0700

    Add IDs for elements

--- src/scheduleapp/view_controller/FXMLCustomersController.java
+++ src/scheduleapp/view_controller/FXMLCustomersController.java
@@ -7,21 +7,78 @@ package scheduleapp.view_controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author Jens Larsen
  */
-public class FXMLCustomersController implements Initializable {
+public class FXMLCustomersController extends Application {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
+    @FXML
+    private Button buttonCalendar;
+
+    @FXML
+    private Button buttonCustomers;
+
+    @FXML
+    private TextField textFieldSearch;
+
+    @FXML
+    private Button buttonSearch;
+
+    @FXML
+    private Button buttonAdd;
+
+    @FXML
+    private TableView<?> tableViewCustomers;
+
+    @FXML
+    private TextField textFieldFirstName;
+
+    @FXML
+    private TextField textFieldLastName;
+
+    @FXML
+    private TextField textFieldAddress;
+
+    @FXML
+    private TextField textFieldAddress2;
+
+    @FXML
+    private TextField textFieldCity;
+
+    @FXML
+    private TextField textFieldState;
+
+    @FXML
+    private TextField textFieldPhone;
+
+    @FXML
+    private TextField textFieldZip;
+
+    @FXML
+    private Button buttonSave;
+
+    @FXML
+    private Button buttonDelete;
+
+    @FXML
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
+        // load customers from database
+        // display in table
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
