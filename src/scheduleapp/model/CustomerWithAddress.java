db7277c60cd5d4d131d0dd492c932a0e5e8214a8
Jens Larsen <dasuperman@gmail.com>
2018-06-10 16:06:52 -0700

    Add CustomerWithAddress object

---
+++ src/scheduleapp/model/CustomerWithAddress.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scheduleapp.model;
+
+/**
+ *
+ * @author Jens Larsen
+ */
+public class CustomerWithAddress {
+
+    private int customerID;
+    private String customerName;
+    private int addressID;
+    private boolean active;
+
+    private String addressId;
+    private String address;
+    private String address2;
+    private String city;
+    private String postalCode;
+    private String phone;
+
+}
