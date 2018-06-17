05fedf1d705fa469fbedd2aff92ce6deddc410be
Jens Larsen <dasuperman@gmail.com>
2018-06-17 12:24:28 -0700

    Make Add button load new customer screen

--- src/scheduleapp/view_controller/FXMLAddCustomerController.java
+++ src/scheduleapp/view_controller/FXMLAddCustomerController.java
@@ -7,14 +7,55 @@ package scheduleapp.view_controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
  *
- * @author dasuperman
+ * @author Jens Larsen
  */
 public class FXMLAddCustomerController implements Initializable {
+
+    @FXML
+    private TextField textFieldName;
+
+    @FXML
+    private TextField textFieldAddress;
+
+    @FXML
+    private TextField textFieldAddress2;
+
+    @FXML
+    private TextField textFieldCity;
+
+    @FXML
+    private TextField textFieldCountry;
+
+    @FXML
+    private TextField textFieldPhone;
+
+    @FXML
+    private Button buttonSave;
+
+    @FXML
+    private Button buttonCancel;
+
+    @FXML
+    private TextField textFieldPostalCode;
+
+    @FXML
+    void cancelButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonClicked(ActionEvent event) {
+
+    }
 
     /**
      * Initializes the controller class.
--- src/scheduleapp/view_controller/FXMLCustomersController.java
+++ src/scheduleapp/view_controller/FXMLCustomersController.java
@@ -5,6 +5,7 @@
  */
 package scheduleapp.view_controller;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +14,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import scheduleapp.model.CustomerWithAddress;
 import scheduleapp.model.Datasource;
 
@@ -75,8 +81,22 @@ public class FXMLCustomersController {
     }
 
     @FXML
-    void addButtonClicked(ActionEvent event) throws ClassNotFoundException, SQLException {
+    void addButtonClicked(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
 
+        FXMLLoader addCustomerLoader = new FXMLLoader();
+        addCustomerLoader.setLocation(getClass().getResource("FXMLAddCustomer.fxml"));
+
+        Parent root = addCustomerLoader.load();
+
+        stage.setScene(new Scene(root));
+
+        stage.setTitle("Add Customer");
+        stage.initModality(Modality.APPLICATION_MODAL);
+
+        stage.setResizable(false);
+
+        stage.showAndWait();
     }
 
     @FXML
