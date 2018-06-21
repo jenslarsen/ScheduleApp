be4b65aeeb52bb7fe1039c29f98ae358cfc545a5
Jens Larsen <dasuperman@gmail.com>
2018-06-21 07:41:14 -0700

    Fix save button to use Datasource.updateAddress method

--- src/scheduleapp/view_controller/FXMLEditCustomerController.java
+++ src/scheduleapp/view_controller/FXMLEditCustomerController.java
@@ -79,6 +79,8 @@ public class FXMLEditCustomerController {
         String phone = textFieldPhone.getText();
         String postalCode = textFieldPostalCode.getText();
         String country = textFieldCountry.getText();
+        int addressId = Datasource.customerBeingEdited.getAddressId();
+        int customerId = Datasource.customerBeingEdited.getCustomerID();
 
         if (name.equals("")) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -116,25 +118,27 @@ public class FXMLEditCustomerController {
             System.out.println("Added " + city + " with ID " + cityId);
         }
 
-        Address addressToAdd = new Address(address, address2, cityId, postalCode, phone);
-        int addressId = Datasource.addAddress(addressToAdd);
-        if (addressId == -1) {
-            System.out.println("Adding address failed!");
+        Address addressToUpdate = new Address(address, address2, cityId, postalCode, phone);
+        addressToUpdate.setAddressId(addressId);
+
+        boolean addressUpdateSuccessful = Datasource.updateAddress(addressToUpdate);
+
+        if (addressUpdateSuccessful != true) {
+            System.out.println("Updating address failed!");
             Stage stage = (Stage) buttonSave.getScene().getWindow();
             stage.close();
             return;
         }
 
         Customer customerToAdd = new Customer(name, addressId);
-        int customerId = Datasource.addCustomer(customerToAdd);
         if (customerId == -1) {
-            System.out.println("Adding customer failed!");
+            System.out.println("Updating customer failed!");
             Stage stage = (Stage) buttonSave.getScene().getWindow();
             stage.close();
             return;
         }
 
-        System.out.println("Added " + name + " with ID " + customerId);
+        System.out.println("Updated " + name + " with ID " + customerId);
 
         Stage stage = (Stage) buttonSave.getScene().getWindow();
         stage.close();
