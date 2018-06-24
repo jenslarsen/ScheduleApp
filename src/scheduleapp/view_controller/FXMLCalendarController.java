215a2877c6267f470b0fee66868135f72ef29203
Jens Larsen <dasuperman@gmail.com>
2018-06-23 17:23:49 -0700

    Add new calendar view

--- src/scheduleapp/view_controller/FXMLCalendarController.java
+++ src/scheduleapp/view_controller/FXMLCalendarController.java
@@ -7,7 +7,11 @@ package scheduleapp.view_controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -15,6 +19,51 @@ import javafx.fxml.Initializable;
  * @author dasuperman
  */
 public class FXMLCalendarController implements Initializable {
+
+    @FXML
+    private Button buttonAdd;
+
+    @FXML
+    private Button buttonEdit;
+
+    @FXML
+    private TableView<?> tableViewCustomers;
+
+    @FXML
+    private TableColumn<?, ?> tableColName;
+
+    @FXML
+    private TableColumn<?, ?> tableColAddress;
+
+    @FXML
+    private TableColumn<?, ?> tableColPhone;
+
+    @FXML
+    private TableColumn<?, ?> tableColPhone1;
+
+    @FXML
+    private Button buttonDelete;
+
+    @FXML
+    private Button buttonCalendar;
+
+    @FXML
+    private Button buttonCustomers;
+
+    private void addButtonClicked() {
+    }
+
+    private void editButtonClicked() {
+    }
+
+    private void deleteButtonClicked() {
+    }
+
+    private void customersButtonClicked() {
+    }
+
+    private void calendarButtonClicked() {
+    }
 
     /**
      * Initializes the controller class.
