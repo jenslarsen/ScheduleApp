9fa73dc73fd264f3ddacdbc33f47d03755b8b353
Jens Larsen <dasuperman@gmail.com>
2018-07-04 10:09:04 -0700

    Add functionality to edit appointments

--- src/scheduleapp/view_controller/FXMLEditAppointmentController.java
+++ src/scheduleapp/view_controller/FXMLEditAppointmentController.java
@@ -5,12 +5,26 @@
  */
 package scheduleapp.view_controller;
 
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import scheduleapp.model.Appointment;
+import scheduleapp.model.CustomerWithAddress;
+import scheduleapp.model.Datasource;
 
 /**
  * FXML Controller class
@@ -38,40 +52,145 @@ public class FXMLEditAppointmentController {
     private DatePicker datePickerEnd;
 
     @FXML
-    private ComboBox<?> comboCustomer;
+    private ComboBox<String> comboCustomer;
 
     @FXML
     private TextField textFieldUrl;
 
     @FXML
-    private ComboBox<?> comboStartHour;
+    private ComboBox<String> comboStartHour;
 
     @FXML
-    private ComboBox<?> comboStartMinute;
+    private ComboBox<String> comboStartMinute;
 
     @FXML
-    private ComboBox<?> comboEndHour;
+    private ComboBox<String> comboEndHour;
 
     @FXML
-    private ComboBox<?> comboEndMinute;
+    private ComboBox<String> comboEndMinute;
 
     @FXML
     private TextField textFieldTitle;
 
+    private List<CustomerWithAddress> customers;
+    ObservableList<String> customerList
+            = FXCollections.observableArrayList();
+
     @FXML
     void cancelButtonClicked(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Cancel Editing Appointment");
+        alert.setContentText("Are you sure?");
 
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            Stage stage = (Stage) buttonCancel.getScene().getWindow();
+            stage.close();
+        }
     }
 
     @FXML
-    void saveButtonClicked(ActionEvent event) {
+    void saveButtonClicked(ActionEvent event) throws ParseException, ClassNotFoundException, SQLException {
+        Appointment newAppointment = new Appointment();
 
+        newAppointment.setTitle(textFieldTitle.getText());
+        newAppointment.setDescription(textFieldDescription.getText());
+        newAppointment.setLocation(textFieldLocation.getText());
+        newAppointment.setUrl(textFieldUrl.getText());
+
+        if (newAppointment.getTitle().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Input error");
+            alert.setHeaderText("You must enter a title");
+            alert.setContentText("Some fields are empty");
+            alert.showAndWait();
+            return;
+        }
+
+        try {
+            String startString = datePickerStart.getValue() + " "
+                    + comboStartHour.getValue() + ":" + comboStartMinute.getValue() + ":00";
+
+            newAppointment.setStart(Timestamp.valueOf(startString));
+
+            String endString = datePickerEnd.getValue() + " "
+                    + comboEndHour.getValue() + ":" + comboEndMinute.getValue() + ":00";
+
+            newAppointment.setEnd(Timestamp.valueOf(endString));
+        } catch (IllegalArgumentException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Time entry error");
+            alert.setHeaderText("You must enter a start and end dates and times");
+            alert.setContentText("Some fields are empty");
+            alert.showAndWait();
+            return;
+        }
+
+        newAppointment.setContact(Datasource.loggedInUser);
+        newAppointment.setCreateDate(new Timestamp(System.currentTimeMillis()));
+        newAppointment.setCreatedBy(Datasource.loggedInUser);
+        newAppointment.setLastUpdate((Timestamp) newAppointment.getCreateDate());
+        newAppointment.setLastUpdateBy(Datasource.loggedInUser);
+
+        int customerIndex = comboCustomer.getSelectionModel().getSelectedIndex();
+
+        try {
+            newAppointment.setCustomerID(customers.get(customerIndex).getCustomerID());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Customer seleciton error");
+            alert.setHeaderText("You must select a customer");
+            alert.setContentText("Some fields are empty");
+            alert.showAndWait();
+            return;
+        }
+        Datasource.updateAppointment(newAppointment);
     }
 
-    /**
-     * Initializes the controller class.
-     */
-    public void initialize() {
-        // TODO
+    public void initialize() throws ClassNotFoundException, SQLException {
+
+        // load customers into dropdown
+        customers = Datasource.getCustomersWithAddresses();
+        List<String> listOfCustomers = new ArrayList();
+        customers.forEach((customer) -> {
+            listOfCustomers.add(customer.getCustomerName());
+        });
+        customerList = FXCollections.observableArrayList(listOfCustomers);
+        comboCustomer.setItems(customerList);
+
+        // populate time dropdowns
+        ObservableList<String> hours = FXCollections.observableArrayList(
+                "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12",
+                "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"
+        );
+
+        ObservableList<String> mins = FXCollections.observableArrayList(
+                "00", "15", "30", "45"
+        );
+
+        comboStartHour.setItems(hours);
+        comboStartMinute.setItems(mins);
+        comboEndHour.setItems(hours);
+        comboEndMinute.setItems(mins);
+
+        textFieldTitle.setText(Datasource.appointmentBeingEdited.getTitle());
+        textFieldDescription.setText(Datasource.appointmentBeingEdited.getDescription());
+        textFieldLocation.setText(Datasource.appointmentBeingEdited.getLocation());
+        datePickerStart.setValue(
+                Datasource.appointmentBeingEdited.getStart()
+                        .toLocalDateTime().toLocalDate());
+        datePickerEnd.setValue(
+                Datasource.appointmentBeingEdited.getEnd()
+                        .toLocalDateTime().toLocalDate());
+
+        int start = Datasource.appointmentBeingEdited.getStart().getHours();
+
+        comboStartHour.getSelectionModel().select(Datasource.appointmentBeingEdited.getStart().getHours());
+        comboStartMinute.getSelectionModel().select(Datasource.appointmentBeingEdited.getStart().getMinutes());
+        comboEndHour.getSelectionModel().select(Datasource.appointmentBeingEdited.getEnd().getHours());
+        comboEndMinute.getSelectionModel().select(Datasource.appointmentBeingEdited.getEnd().getMinutes());
+
+        textFieldUrl.setText(Datasource.appointmentBeingEdited.getUrl());
     }
 }
