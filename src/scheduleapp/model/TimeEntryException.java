5787ac2225cc38b8a3c993c2f721d1780482d5e1
Jens Larsen <dasuperman@gmail.com>
2018-07-15 12:46:39 -0700

    Add TimeFormatException to handle time entry errors

---
+++ src/scheduleapp/model/TimeEntryException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scheduleapp.model;
+
+/**
+ *
+ * @author Jens Larsen
+ */
+public class TimeEntryException extends Exception {
+
+    public TimeEntryException(String message) {
+        super(message);
+    }
+}
